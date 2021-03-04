49d0a048fbcfa75149895124c5989d4506b09088
Frank Gehann <gehannf@gmail.com>
2021-03-04 23:03:23 +0100

    Removed metrics.net and improved the rabbitmq example app to load config settings from appsettings.json.

--- examples/RabbitMq/Program.cs
+++ examples/RabbitMq/Program.cs
@@ -1,12 +1,8 @@
-using App.Metrics;
-using App.Metrics.AspNetCore;
-using BusLane.Serializing.Json;
 using BusLane.Serializing.MessagePack;
 using BusLane.Transport.RabbitMQ;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 
 namespace RabbitMq
 {
@@ -19,28 +15,25 @@ namespace RabbitMq
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureMetrics(
-                    builder =>
-                    {
-                        builder.Report.ToConsole(
-                            options => { options.FlushInterval = TimeSpan.FromSeconds(5); });
-                    })
                 .ConfigureServices(
                     (hostContext, services) =>
                     {
+                        string rmqHost = hostContext.Configuration.GetValue("Rabbitmq:HostName", "127.0.0.1");
+                        int rmqPort = hostContext.Configuration.GetValue("Rabbitmq:Port", 5672);
+                        
                         services.AddRabbitMqMessageProducer(
                             connectionConfig =>
                             {
-                                connectionConfig.HostName = "127.0.0.1";
-                                connectionConfig.Port = 5672;
+                                connectionConfig.HostName = rmqHost;
+                                connectionConfig.Port = rmqPort;
                             },
                             messageSerializer: new MessagePackMessageSerializer());
 
                         services.AddRabbitMqMessageConsumer(
                             connectionConfig =>
                             {
-                                connectionConfig.HostName = "127.0.0.1";
-                                connectionConfig.Port = 5672;
+                                connectionConfig.HostName = rmqHost;
+                                connectionConfig.Port = rmqPort;
                             },
                             messageDeserializer: new MessagePackMessageDeserializer());
 
--- examples/RabbitMq/Worker.cs
+++ examples/RabbitMq/Worker.cs
@@ -1,10 +1,9 @@
-using App.Metrics;
-using App.Metrics.Meter;
-using App.Metrics.Timer;
 using BusLane.Consuming;
 using BusLane.Producing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,62 +11,28 @@ namespace RabbitMq
 {
     public class Worker : BackgroundService
     {
+        private readonly IConfiguration _Config;
         private readonly IMessageConsumer _MessageConsumer;
         private readonly IMessageProducer _MessageProducer;
-        private readonly IMetrics _Metrics;
-        private readonly TimerOptions _PublishTimer;
-        private readonly MeterOptions _PublishMeter;
-        private readonly MeterOptions _ConsumeMeter;
 
         public Worker(
+            IConfiguration config,
             IMessageConsumer messageConsumer,
-            IMessageProducer messageProducer,
-            IMetrics metrics)
+            IMessageProducer messageProducer)
         {
+            _Config = config ?? throw new ArgumentNullException(nameof(config));
             _MessageConsumer = messageConsumer ?? throw new ArgumentNullException(nameof(messageConsumer));
             _MessageProducer = messageProducer ?? throw new ArgumentNullException(nameof(messageProducer));
-            _Metrics = metrics ?? throw new ArgumentNullException(nameof(metrics));
-
-            _PublishMeter = new MeterOptions
-            {
-                Name = "Publish Message/s",
-                MeasurementUnit = Unit.Calls,
-                RateUnit = TimeUnit.Seconds
-            };
-
-            _PublishTimer = new TimerOptions
-            {
-                Name = "Publish Message latency/ms",
-                MeasurementUnit = Unit.Requests,
-                DurationUnit = TimeUnit.Milliseconds,
-                RateUnit = TimeUnit.Milliseconds
-            };
-            
-            _ConsumeMeter = new MeterOptions
-            {
-                Name = "Consume Message/s",
-                MeasurementUnit = Unit.Calls,
-                RateUnit = TimeUnit.Seconds
-            };
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             await _MessageConsumer.SubscribeAsync<TestMessage>("test", HandleMessage, stoppingToken);
 
-            for (int taskCount = Environment.ProcessorCount; taskCount > 0; taskCount--)
+            List<Task> messageProducerWorker = new List<Task>();
+            for (int taskCount = _Config.GetValue("Producer:NumberOfTasks", 1); taskCount > 0; taskCount--)
             {
-                await Task.Run(
-                    () =>
-                    {
-                        while (!stoppingToken.IsCancellationRequested)
-                        {
-                            _Metrics.Measure.Timer.Time(
-                                _PublishTimer,
-                                async () => await PublishMessageAsync(stoppingToken));
-                        }
-                    },
-                    stoppingToken);
+                messageProducerWorker.Add(Task.Run(() => PublishMessageAsync(stoppingToken), stoppingToken));
             }
 
             while (!stoppingToken.IsCancellationRequested)
@@ -78,25 +43,21 @@ namespace RabbitMq
 
         private async Task PublishMessageAsync(CancellationToken token)
         {
-            await _MessageProducer.PublishAsync(
-                "test",
-                new TestMessage()
-                {
-                    Test = "foo"
-                },
-                token);
-            
-            _Metrics.Measure.Meter.Mark(_PublishMeter);
+            while (!token.IsCancellationRequested)
+            {
+                await _MessageProducer.PublishAsync(
+                    "test",
+                    new TestMessage()
+                    {
+                        Test = "foo"
+                    },
+                    token);
+            }
         }
 
-        private async Task HandleMessage(TestMessage message, CancellationToken token)
+        private Task HandleMessage(TestMessage message, CancellationToken token)
         {
-            await Task.Run(
-                () =>
-                {
-                    _Metrics.Measure.Meter.Mark(_ConsumeMeter);
-                },
-                token);
+            return Task.CompletedTask;
         }
     }
 }
